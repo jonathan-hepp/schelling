c6b46c7cc111f920305c8d45ebbe892265cb5cd4
Jonathan Hepp <jonathan.hepp@gmail.com>
2016-05-21 00:36:06 -0300

    Added configuration service and general style changes

--- static/js/schelling.js
+++ static/js/schelling.js
@@ -1,20 +1,25 @@
-var app = angular.module('schellingApp',[]);
+var app = angular.module('app',[]);
 
-app.controller('CanvasController', function($scope, $timeout, $interval) {
-    $scope.canvas = {width: 60, height: 40, blockSize: 10, blanksRatio: 10,
-                     firstColorRatio: 50, secondColorRatio: 50, threshold: 30,
-                     speed: "500", fillStroke: true, round: 0, unsatisfied: 0,
-                     firstColorName: "Blue", secondColorName: "Red"};
+// Default configuration service
+app.value('defaults', {width: 60, height: 40, blockSize: 10, blanksRatio: 10,
+                 firstColorRatio: 50, secondColorRatio: 50, threshold: "3",
+                 speed: "100", fillStroke: true, round: 0, unsatisfied: 0,
+                 firstColorName: "Blue", secondColorName: "Yellow",
+                 colors: ["Black", "Blue", "Cyan", "Brown", "Gray", "Green",
+                 "Maroon", "Olive", "Orange", "Pink", "Red", "Teal", "Violet", "Yellow"]});
 
-    $scope.colors = ["Black", "Blue", "Cyan", "Brown", "Gray", "Green", "Maroon", "Olive", "Orange", "Pink", "Red", "Teal", "Violet", "Yellow"];
+app.controller('SimulationController', function($scope, $timeout, $interval, defaults) {
+    $scope.conf = defaults;
 
     function runSimulation(){
-        $scope.canvas.round++;
-        var unsfd = listUnsatisfied($scope.board, $scope.canvas.threshold);
-        $scope.canvas.unsatisfied = (unsfd.length - $scope.blanksTotal) / ($scope.canvas.width * $scope.canvas.height) * 100;
-        if(unsfd.length > $scope.blanksTotal){
-            swapAgents(unsfd, $scope.board);
+        var unsatisfiedAndBlanks = listUnsatisfiedAndBlanks($scope.board, $scope.conf.threshold);
+        $scope.conf.round++;
+        $scope.conf.unsatisfied = (unsatisfiedAndBlanks.length - $scope.blanksTotal) / ($scope.conf.width * $scope.conf.height) * 100;
+        // If there are still unsatisfied agenst, update their position and redraw the board
+        if(unsatisfiedAndBlanks.length > $scope.blanksTotal){
+            swapAgents(unsatisfiedAndBlanks, $scope.board);
             $scope.draw();
+        // Otherwise, the simulation is done
         }else{
             $scope.stop();
             alert("Finished!")
@@ -22,50 +27,73 @@ app.controller('CanvasController', function($scope, $timeout, $interval) {
     }
 
     $scope.initializeBoard = function(){
-            var rows = $scope.canvas.width;
-            var cols = $scope.canvas.height;
-            $scope.blanksTotal = rows * cols * ($scope.canvas.blanksRatio/100);
-            var pInt = function(e){return parseInt(e);};
-            var boardArray = [...(""+A).repeat((rows * cols - $scope.blanksTotal) * ($scope.canvas.firstColorRatio /100)).split("").map(pInt),
-                          ...(""+B).repeat((rows * cols - $scope.blanksTotal) * (1 - ($scope.canvas.firstColorRatio/100))).split("").map(pInt),
-                          ...(""+E).repeat($scope.blanksTotal).split("").map(pInt)];
-            shuffleArray(boardArray);
+            var rows = $scope.conf.width;
+            var cols = $scope.conf.height;
+            $scope.blanksTotal = rows * cols * ($scope.conf.blanksRatio/100);
+
+            /*
+            The agents array is initially populated with the distribution of the colors based on the configuration values.
+
+            Note: The array construction will be optimized when major browsers fully support ES6's Array comprehension.
+            Currently, the code below only works on Firefox.
+
+            var boardArray = [...[for (i of [...Array((rows * cols - $scope.blanksTotal) * ($scope.conf.firstColorRatio / 100)).keys()]) A],
+                              ...[for (i of [...Array((rows * cols - $scope.blanksTotal) * (1 - ($scope.conf.firstColorRatio / 100))).keys()]) B],
+                              ...[for (i of [...Array($scope.blanksTotal).keys()]) E]];
+
+            Until then, the best approach is to use the String.repeat() functionality.
+            */
+
+            var toInt = function(e){ return parseInt(e); };
+            var agentsArray = [...(""+A).repeat((rows * cols - $scope.blanksTotal) * ($scope.conf.firstColorRatio / 100)).split("").map(toInt),
+                         ...(""+B).repeat((rows * cols - $scope.blanksTotal) * (1 - ($scope.conf.firstColorRatio / 100))).split("").map(toInt),
+                         ...(""+E).repeat($scope.blanksTotal).split("").map(toInt)];
+
+            shuffleArray(agentsArray);
+
+            // Now that we have the agents shuffled, it's time to build the board
             $scope.board = [];
             for(var i = 0; i < rows; i++){
-                $scope.board.push(boardArray.splice(0,cols));
+                $scope.board.push(agentsArray.splice(0, cols));
             }
             $scope.draw();
-            $scope.canvas.unsatisfied = countUnsatisfied($scope.board, $scope.canvas.threshold) / ($scope.canvas.width * $scope.canvas.height) * 100;
-            $scope.canvas.round = 0;
+            $scope.conf.unsatisfied = countUnsatisfied($scope.board, $scope.conf.threshold) / ($scope.conf.width * $scope.conf.height) * 100;
     }
-
 
     $scope.draw = function() {
         $timeout(function(){
             var canvas = document.getElementById("canvas");
             if (canvas.getContext) {
                 var ctx = canvas.getContext("2d");
+                var blockSize = $scope.conf.blockSize;
+                var palette = ["white", $scope.conf.firstColorName, $scope.conf.secondColorName];
                 ctx.strokeStyle = "black";
-                ctx.lineWidth = 1;
-                var palette = ["white", $scope.canvas.firstColorName, $scope.canvas.secondColorName];
-                var blockSize = $scope.canvas.blockSize;
-                for(var x = 0; x < $scope.canvas.width; x++){
-                    for (var y = 0; y < $scope.canvas.height; y++){
+                ctx.lineWidth = 0.5;
+                for(var x = 0; x < $scope.conf.width; x++){
+                    for (var y = 0; y < $scope.conf.height; y++){
                         ctx.fillStyle = palette[$scope.board[x][y]];
-                        ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
-                        if($scope.canvas.fillStroke)
-                            ctx.strokeRect(x*blockSize, y*blockSize, blockSize, blockSize);
+                        ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+                        if($scope.conf.fillStroke)
+                            ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
                     }
                 }
+            }else{
+                alert("This browser doesn't support canvas technology. In order to use this application, you'll have to either update your browser or use another one.");
             }
         });
     }
 
     $scope.start = function(){
-        $scope.intervalID = $interval(function(){runSimulation()}, $scope.canvas.speed);
+        $("#btnStart").prop("disabled", true);
+        $("#btnStop").prop("disabled", false);
+        $("#btnReset").prop("disabled", true);
+        $scope.intervalID = $interval(function(){ runSimulation() }, $scope.conf.speed);
     }
 
     $scope.stop = function(){
+        $("#btnStart").prop("disabled", false);
+        $("#btnStop").prop("disabled", true);
+        $("#btnReset").prop("disabled", false);
         $interval.cancel($scope.intervalID);
     }
 
@@ -94,7 +122,7 @@ function swapAgents(array, board){
     }
 }
 
-function listUnsatisfied(board, threshold){
+function listUnsatisfiedAndBlanks(board, threshold){
     var unsatisfied = [];
     for(var x = 0; x < board.length; x++){
         for(var y = 0; y < board[x].length; y++){
@@ -110,7 +138,7 @@ function countUnsatisfied(board, threshold){
     var unsatisfied = 0;
     for(var x = 0; x < board.length; x++){
         for(var y = 0; y < board[x].length; y++){
-            if(countSurroundings(board, x, y) < threshold){
+            if(countSurroundings(board, x, y) > threshold){
                 unsatisfied++;
             }
         }
@@ -130,7 +158,7 @@ function countSurroundings(board, x, y){
             }
         }
     }
-    return count / 8 * 100;
+    return count;
 }
 
 function shuffleArray(array) {
